23c9da4f27069081ddae754944799f63b5b35c41
Joel Panjaitan <joel.martuaraja@praktis.co>
2024-09-02 23:51:12 +0700

    update for text py bank account class

---
+++ text.py
@@ -0,0 +1,37 @@
+class Bank_Account:
+    def __init__(self, holder_name, account_number, balance) -> None:
+        self.holder_name= holder_name
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount>0:
+            self.balance +=amount
+            print(f"Deposited: ${amount:.3f}")
+        else: 
+            print(f"Deposit must be positive!")
+
+    def withdraw(self, amount):
+        if (amount>0):
+            if (self.balance>=amount):
+                self.balance -= amount
+                print(f"Withdrew: ${amount:.1f}")
+            else:
+                print("Insufficient funds")
+        else :
+            print("Withdrawal amount invalid")
+    
+    def get_balance(self):
+        return f"Current balance: ${self.balance}"
+    
+    def account_info(self):
+        return (f"Account Holder: ${self.holder_name}\n"
+                f"Account Number: ${self.account_number}\n"
+                f"Balance: ${self.balance}")
+    
+
+account = Bank_Account("Terrence Wiliam","89912311",10)
+account.deposit(50)
+account.withdraw(40)
+account.withdraw(20)
+print(account.account_info())
