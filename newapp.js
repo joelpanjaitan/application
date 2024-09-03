3744ab57a7d0092de988de7a46d900c3e94ab71a
Joel Panjaitan <joel.martuaraja@praktis.co>
2024-09-03 20:56:03 +0700

    update for newapp js file

--- newapp.js
+++ newapp.js
@@ -1,3 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+let pathFolder = path.join(__dirname, "storage", "files");
+let nameFile = "message.log";
+let pathFile = path.join(pathFolder, nameFile);
+fs.access(pathFile, fs.constants.F_OK, (err) => {
+  if (err) {
+    console.log(`${nameFile} tidak ada.`);
+  } else {
+    console.log(`${nameFile} ada.`);
+  }
+});
+
+let text = `Klik link berikut untuk mengajuan motor Honda: {url}`;
+let text2 = `Gunakan kode voucher {code} dapatkan diskon s.d 1 juta untuk pemesanan motor`;
+let result = [];
+const matches = /\{([^{}]+)\}/g;
+let match;
+while ((match = matches.exec(text2)) !== null) {
+  result.push(match[1]);
+}
+// console.log(result);
+
 const dayjs = require("dayjs");
 const duration = require("dayjs/plugin/duration");
 
@@ -6,18 +29,24 @@ dayjs.extend(duration);
 const unixTime = 19999999;
 
 let date = dayjs.unix(unixTime);
-const newDate = date.addTime(90, "minute");
+const targetDate = dayjs("2024-01-01 00:00:00");
+const minuteDifference = targetDate.diff(date, "minute");
+const newDate = date.add(minuteDifference, "minute");
 const resultDate = newDate.format("YYYY-MM-DD hh:mm:ss");
-console.log(resultDate);
-import validator from "validator";
+// console.log(resultDate);
+// const newDate = date.add(90, "minute");
 let StaffData = [
   { name: "1299", gender: "F", birthDate: "2000-01-01" },
   { name: "2", gender: "male", birthDate: "2000-01-02" },
   { name: "3", gender: "male ", birthDate: "2000-01-02" },
   { name: "4", gender: "male", birthDate: "2000-01-02" },
 ];
-
-let result = [];
+// console.log(
+//   StaffData.map(
+//     (object) => `${object.name} ${object.gender} ${object.birthDate}`
+//   )
+// );
+// let result = [];
 function validateObject(obj) {
   let { customerName, customerPhone, customerEmail } = obj;
   const validCode = !obj.code;
