f6856c846c640ece81e339ece7fc28c276b042b5
Joel Panjaitan <joel.martuaraja@praktis.co>
2024-09-19 20:37:08 +0700

    update for question 1

--- question1.py
+++ question1.py
@@ -2,6 +2,8 @@ def valid_palindrome(value:str):
     # check if characters if non alphanumeric
     resultCharacter = ''.join(value.lower() for letter in value if letter.isalnum());
     #if final character the same as reverse then true else false
-    return resultCharacter==resultCharacter[::-1]
+    return (resultCharacter==resultCharacter[::-1])
 
-print(valid_palindrome('A man, a plan, a canal: Panama'))
+print(valid_palindrome('A man, a plan, a canal: Panama'));
+print(valid_palindrome("race a car"));
+print(valid_palindrome(""));
